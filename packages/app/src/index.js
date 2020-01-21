2d0ff3f212f3207b8b46a7fbcbed291adba896da
Jun Yamasaki <tjun.yamasaki@gmail.com>
2020-01-21 19:04:29 -0300

    publish just dist content

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <h1>App test ver 2</h1>
+  <h1>BUNDLED APP TEST 1</h1>
 );
 
 export default App;
--- packages/component/src/index.js
+++ packages/component/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Component = () => (
-  <h2>Component test ver 1</h2>
+  <h2>BUNDLED COMPONENT VER 1</h2>
 );
 
 export default Component;
